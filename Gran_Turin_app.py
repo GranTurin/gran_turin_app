1b6d567296402b15e211a7762bc1fb86a9a3ee43
GranTurin <granturindelivery@gmail.com>
2025-12-21 23:34:10 -0300

    Update Gran_Turin_app.py

--- Gran_Turin_app.py
+++ Gran_Turin_app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import urllib.parse
 
+st.set_page_config(
+    page_title="Gran Turin",
+    page_icon="logo.png", # Isso coloca a logo na aba do navegador
+)
+
 # LINK DA SUA PLANILHA (Cole o link do CSV aqui)
 URL_PLANILHA = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBai98jFvBGaS_TM0Qaao5bGanhR85VbvSuFFJvbha1DW5gXJlyXXqEiq3dUgVvQTqplDcG3jQqqLG/pub?output=csv"
 def carregar_dados():
@@ -27,6 +32,7 @@ if st.button("Enviar Pedido"):
     msg = f"Olá! Pedido de {nome}: {carne} com {', '.join(acomp)}"
     link = f"https://wa.me/5521986577315?text={urllib.parse.quote(msg)}"
     st.link_button("Ir para o WhatsApp", link)
+
 
 
 
